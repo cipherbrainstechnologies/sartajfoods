16cbeb18add07d6e855105ca42baefcfc64b5af7
KrunalSWB <krunal@silverwebbuzz.com>
2023-12-04 18:30:52 +0530

    changes

--- app/Http/Middleware/ModifyHeadersMiddleware.php
+++ app/Http/Middleware/ModifyHeadersMiddleware.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ModifyHeadersMiddleware
 {
@@ -19,9 +20,16 @@ class ModifyHeadersMiddleware
     public function handle(Request $request, Closure $next)
     {
         $response = $next( $request );
-        $response->header( 'Access-Control-Allow-Origin', '*' );
-        $response->header( 'Access-Control-Allow-Headers', 'Origin, Content-Type' );
-    
+        if ($response instanceof StreamedResponse) {
+            // For StreamedResponse, use headers property
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type');
+        } else {
+            // For other responses, use header() method
+            $response->header('Access-Control-Allow-Origin', '*');
+            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type');
+        }
+   
         return $response;
     }
 }
