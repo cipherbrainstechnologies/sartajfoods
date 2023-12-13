8abf18b6379893a2e5bbbd7f2f4079bdb3d099f8
Vishnu-Prajapati <vishnu@silverwebbuzz.com>
2023-12-13 10:36:39 +0530

    fixed

--- app/CentralLogics/product.php
+++ app/CentralLogics/product.php
@@ -283,11 +283,9 @@ class ProductLogic
 
     public static function get_most_reviewed_products($limit = 10, $offset = 1,$take=null){
         $paginator = Product::active()
-            ->with(['rating'  => function ($query) {
-                $query->orderBy('average', 'desc');
-            }, 'active_reviews','manufacturer', 'soldProduct'])
-            ->withCount('active_reviews');
-            // ->orderBy('active_reviews_count', 'desc');
+            ->with(['rating', 'active_reviews','manufacturer', 'soldProduct'])
+            ->withCount('active_reviews')
+            ->orderBy('active_reviews_count', 'desc');
             if(!is_null($take)){
                 $limit = $take;                    
             }
