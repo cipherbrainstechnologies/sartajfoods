c3cf0ee94f49eaa07e216fea99f89dbbbe5ae8ce
mukesh <mukesh@silverwebbuzz.com>
2023-12-18 12:11:07 +0530

    changes.

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -446,14 +446,11 @@ class ProductController extends Controller
      * @return JsonResponse
      */
     public function get_rated_three_products(){ 
-        echo "dsf";die;
         $products = ProductLogic::get_most_reviewed_products(3);
         $products = Helpers::product_data_formatting($products['products'], true);
         ProductLogic::cal_rating_and_review($products);
         ProductLogic::getSoldProducts($products);
-        usort($products, function ($a, $b) {
-            return $b['overall_rating'] - $a['overall_rating'];
-        });
+        
         return response()->json($products, 200);
     }
 
