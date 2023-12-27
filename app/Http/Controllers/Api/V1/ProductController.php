dd651cd299bc5f0bebdf7e522a4dfc994fd01d1e
mukesh <mukesh@silverwebbuzz.com>
2023-12-27 15:05:51 +0530

    changes.

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -487,7 +487,7 @@ class ProductController extends Controller
      */
     public function get_rated_three_products(){ 
         $products = ProductLogic::get_most_reviewed_products(3);
-        $products = Helpers::product_data_formatting($products['products'], true);
+        // $products = Helpers::product_data_formatting($products['products'], true);
         // ProductLogic::cal_rating_and_review($products);
         // ProductLogic::getSoldProducts($products);
         // usort($products, function ($a, $b) {
