1e4389f82471776f0e4b8f64cecd25b4a64a993b
mukesh <mukesh@silverwebbuzz.com>
2023-12-29 12:36:04 +0530

    changes.

--- resources/views/admin-views/order/order-view.blade.php
+++ resources/views/admin-views/order/order-view.blade.php
@@ -410,7 +410,7 @@
                             <tbody>
                                 @foreach($order->history as $history)
                                 <tr>
-                                    <td>{{ empty($order['delivery_date']) ? date('d M Y',strtotime($history['created_at'])) :$order['delivery_date'] }}</td>
+                                    <td>{{ ($history['created_at']) ? date('d M Y',strtotime($history['created_at'])) }}</td>
                                     <td>{{ $history['comment'] }}</td>
                                     <td>{{ str_ireplace( array('_'), ' ', $history['status']) }}</td>
                                     <td class="text-center">{{ ($history['is_customer_notify']) ? 'Yes' : 'No' }}</td>
