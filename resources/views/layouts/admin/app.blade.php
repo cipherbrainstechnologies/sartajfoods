3d47a9c5d6332ba8953f686fa4b769bc202f19d6
mukesh <mukesh@silverwebbuzz.com>
2024-01-01 09:22:06 +0530

    changes.

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <!-- Title -->
     <title>@yield('title')</title>
     <!-- Favicon -->
