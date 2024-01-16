f9f07936c0507a51e89b577bcc4f196e67641d35
mukesh <mukesh@silverwebbuzz.com>
2024-01-16 10:23:20 +0530

    related product image display.

--- app/Model/RelatedProducts.php
+++ app/Model/RelatedProducts.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class RelatedProducts extends Model
 {
     use HasFactory;
-
+    protected $appends = ['image'];
     protected $fillable = [
         'product_id',
         'related_product_id',
@@ -22,5 +22,19 @@ class RelatedProducts extends Model
     public function relatedProduct()
     {
         return $this->belongsTo(Product::class, 'related_product_id');
+    }
+
+    public function getImageAttribute()
+    {
+        if (!empty($this->relatedProduct->image)) {
+            $modifiedImages = [];
+            $images = json_decode($this->relatedProduct->image,true);
+            foreach ($images as $image) {
+                $modifiedImages[] = config('app.url') . '/storage/products/' . $image;
+            }
+            return $modifiedImages;
+        }
+        return null;
+       
     }
 }
