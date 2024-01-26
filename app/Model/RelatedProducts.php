481bd62af02b3369f70c9993d397191796878a17
mukesh <mukesh@silverwebbuzz.com>
2024-01-26 11:37:12 +0530

    changes in related products.

--- app/Model/RelatedProducts.php
+++ app/Model/RelatedProducts.php
@@ -32,29 +32,46 @@ class RelatedProducts extends Model
     }
     
 
-    public function getImageAttribute()
-    {
-        $modifiedImages = [];
-        if(is_array($this->relatedProduct->image) && !empty($this->relatedProduct->image)){
-            foreach ($this->relatedProduct->image as $image) {
-                $modifiedImages[] = config('app.url') . '/storage/product/image/' . $image;
-            }
-            $this->relatedProduct->image = $modifiedImages;
-             return $modifiedImages;
-        }
-        // if (!empty($this->relatedProduct->image)) {
-           
-        //     $images = json_decode($this->relatedProduct->image,true);
-        //     foreach ($images as $image) {
-        //         $modifiedImages[] = config('app.url') . '/storage/product/image/' . $image;
-        //     }
-        //     $this->relatedProduct->image = $modifiedImages;
-        //      return $modifiedImages;
-        //     // return config('app.url') . '/storage/products/' . $this->relatedProduct->image;
-        // }
-        return null;
+    // public function getImageAttribute()
+    // {
+    //     $modifiedImages = [];
+    //     if (!empty($this->relatedProduct->image)) {
+    //         $images = json_decode($this->relatedProduct->image,true);
+    //         foreach ($images as $image) {
+    //             $modifiedImages[] = config('app.url') . '/storage/product/image/' . $image;
+    //         }
+    //         $this->relatedProduct->image = $modifiedImages;
+    //          return $modifiedImages;
+    //         // return config('app.url') . '/storage/products/' . $this->relatedProduct->image;
+    //     }
+    //     return null;
        
+    // }
+
+
+    public function getImageAttribute()
+{
+    $modifiedImages = [];
+
+    // Check if $this->relatedProduct->image is a non-empty string
+    if(!empty($this->relatedProduct)){
+        if (is_string($this->relatedProduct->image) && !empty($this->relatedProduct->image)) {
+            $images = json_decode($this->relatedProduct->image, true);
+
+            // Check if $images is an array
+            if (is_array($images)) {
+                foreach ($images as $image) {
+                    $modifiedImages[] = config('app.url') . '/storage/product/image/' . $image;
+                }
+
+                $this->relatedProduct->image = $modifiedImages;
+                return $modifiedImages;
+            }
+        }
     }
+    return null;
+}
+
 
     
 
