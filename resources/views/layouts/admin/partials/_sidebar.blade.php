7c7cbf23ab5db8de867c532a6aba0c9ef8777e1c
mukesh <mukesh@silverwebbuzz.com>
2024-01-31 02:13:25 +0530

    changes.

--- resources/views/layouts/admin/partials/_sidebar.blade.php
+++ resources/views/layouts/admin/partials/_sidebar.blade.php
@@ -253,7 +253,13 @@
                                         </a>
                                     </li>
 
-                                   
+                                    <li class="nav-item {{Request::is('admin/category/add-sub-sub-category')?'active':''}}">
+                                        <a class="nav-link " href="{{route('admin.category.add-sub-sub-category')}}"
+                                           title="{{translate('sub_categories')}}">
+                                            <span class="tio-circle nav-indicator-icon"></span>
+                                            <span class="text-truncate">{{translate('sub_categories')}}</span>
+                                        </a>
+                                    </li>
 
                                     <li class="nav-item {{Request::is('admin/category/add-sub-category')?'active':''}}">
                                         <a class="nav-link " href="{{route('admin.category.add-sub-category')}}"
@@ -263,13 +269,7 @@
                                         </a>
                                     </li>
 
-                                    <li class="nav-item {{Request::is('admin/category/add-sub-sub-category')?'active':''}}">
-                                        <a class="nav-link " href="{{route('admin.category.add-sub-sub-category')}}"
-                                           title="{{translate('sub_categories')}}">
-                                            <span class="tio-circle nav-indicator-icon"></span>
-                                            <span class="text-truncate">{{translate('sub_categories')}}</span>
-                                        </a>
-                                    </li>
+                                    
 
                                     
                                 </ul>
