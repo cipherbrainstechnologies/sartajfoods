09ab35c44ae4a92359ce6546d6070d356e3064d5
mukesh <mukesh@silverwebbuzz.com>
2024-02-02 11:51:54 +0530

    changes in route.

--- app/Http/Controllers/Api/V1/PaypalPaymentController.php
+++ app/Http/Controllers/Api/V1/PaypalPaymentController.php
@@ -161,8 +161,8 @@ class PaypalPaymentController extends Controller
             ->setDescription($tr_ref);
 
         $redirect_urls = new RedirectUrls();
-        $redirect_urls->setReturnUrl(URL::route('paypal-status', ['callback' => $callback, 'transaction_reference' => $tr_ref]))
-            ->setCancelUrl(URL::route('payment-fail', ['callback' => $callback, 'transaction_reference' => $tr_ref]));
+        $redirect_urls->setReturnUrl(URL::route('\api\V1\paypal-status', ['callback' => $callback, 'transaction_reference' => $tr_ref]))
+            ->setCancelUrl(URL::route('\api\V1\payment-fail', ['callback' => $callback, 'transaction_reference' => $tr_ref]));
         echo $redirect_urls;die;
         $payment = new Payment();
         $payment->setIntent('Sale')
