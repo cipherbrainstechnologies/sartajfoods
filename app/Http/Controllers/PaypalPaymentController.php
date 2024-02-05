231276ade8ac2cf2f6cd88042d4a2ee9fcb6bdb2
mukesh <mukesh@silverwebbuzz.com>
2024-02-05 11:29:55 +0530

    paypal payment integration

--- app/Http/Controllers/PaypalPaymentController.php
+++ app/Http/Controllers/PaypalPaymentController.php
@@ -65,10 +65,10 @@ class PaypalPaymentController extends Controller
 
     public function payWithpaypal(Request $request)
     {
-        echo "dsfs";die;
         $order_amount = $request['order_amount'];
         $customerId = !empty($request['customer_id']) ? $request['customer_id'] : auth()->user()->id;
-        $customer = User::find($request['customer_id']);
+        $customer = User::find($customerId);
+        echo "<pre>";print_r($customer);die;
         $callback = $request['callback'];
 
         $tr_ref = Str::random(6) . '-' . rand(1, 1000);
