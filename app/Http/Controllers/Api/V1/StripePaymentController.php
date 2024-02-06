f2c09eb59f2d8bc0c60a4d41f95113aed8b36d60
mukesh <mukesh@silverwebbuzz.com>
2024-02-06 09:42:49 +0530

    changes.

--- app/Http/Controllers/Api/V1/StripePaymentController.php
+++ app/Http/Controllers/Api/V1/StripePaymentController.php
@@ -11,6 +11,7 @@ use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
+use Stripe\PaymentIntent;
 use Stripe\Charge;
 use Stripe\Stripe;
 
