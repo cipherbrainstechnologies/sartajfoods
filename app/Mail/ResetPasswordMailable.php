03a96aef740f02f92f7fd1569f042136cafb7778
cipherbrainstechnologies <info@cipherbrains.com>
2024-02-21 11:55:26 +0530

    changes.

--- app/Mail/ResetPasswordMailable.php
+++ app/Mail/ResetPasswordMailable.php
@@ -15,50 +15,26 @@ class ResetPasswordMailable extends Mailable
     use Queueable, SerializesModels;
     public $user;
     public $token;
+    public $AdminMail;
+    public $businessName;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(User $user, $token)
+    public function __construct(User $user, $token,$AdminMail,$businessName)
     {
         //
         $this->user = $user;
         $this->token = $token;
+        $this->businessName = $businessName;
+        $this->AdminMail = $AdminMail;
     }
 
-    /**
-     * Get the message envelope.
-     *
-     * @return \Illuminate\Mail\Mailables\Envelope
-     */
-    public function envelope()
+    public function build()
     {
-        return new Envelope(
-            subject: 'Password Reset Sartaj Foods',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     *
-     * @return \Illuminate\Mail\Mailables\Content
-     */
-    public function content()
-    {
-        return new Content(
-            view: 'email-templates.opening-reset-mail',
-            
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array
-     */
-    public function attachments()
-    {
-        return [];
+        return $this->from($this->AdminMail, $this->businessName)
+                ->subject('Password Reset Sartaj Foods')
+                ->view('email-templates.opening-reset-mail');
     }
 }
