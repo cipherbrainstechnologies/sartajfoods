60cd9f2a8d96ee01ca92d71ce114d41c406c695e
Love <107467969+nisargcipher@users.noreply.github.com>
2024-08-31 10:46:21 +0530

    notifyme

---
+++ app/NotifyMe.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NotifyMe extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'notified',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
